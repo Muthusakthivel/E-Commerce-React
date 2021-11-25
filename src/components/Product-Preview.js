61cbd703e25eaaec752e84eefb25f137e357b572
Muthusakthivel <smuthukumar1014@gmail.com>
2021-11-25 12:23:53 +0530

    update

--- src/components/Product-Preview.js
+++ src/components/Product-Preview.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
 
 class ProductPreview extends Component {
-    render() {
-        return (
-            <div className="single-wid-product">
-                <a href="single-product.html"><img src={require("../images/product-thumb-1.jpg").default}
-                    className="product-thumb"/></a>
-                <h2><a href="single-product.html">Sony Smart TV - 2015</a></h2>
-                <div className="product-wid-rating">
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                </div>
-                <div className="product-wid-price">
-                    <ins>$400.00</ins> <del>$425.00</del>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    let productPreview = this.props.preview.map((value, index) => {
+      return (
+        <div class="single-wid-product" key={index}>
+          <a>
+            <img src={value.image} alt="" class="product-thumb" />
+          </a>
+          <h2><a>{value.name}</a></h2>
+          <div class="product-wid-rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+          <div class="product-wid-price">
+            <ins>{value.discount_price}</ins> <del>{value.actual_price}</del>
+          </div>
+        </div>
+      )
+    })
+
+    return (
+      productPreview
+    )
+  }
 }
+
 export default ProductPreview;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,60 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { withRouter } from 'react-router-dom';
 
-class Product extends Component{
-    render(){
-        return(
-            <div className="single-product">
+class Product extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      productList : []
+    }
+  }
+
+  seeDetail(value){
+    this.props.history.push("/product", value.id);
+  }
+
+  addProduct(value){
+    this.state.productList.push(value);
+    this.setState({
+      productList : this.state.productList
+    })
+    localStorage.setItem("product-list", JSON.stringify(this.state.productList));
+  }
+
+
+  render() {
+    let products = this.props.products.map((value, index) => {
+      return (
+        <div class="col-md-3">
+          <div className="single-product" key={index}>
             <div className="product-f-image">
-                <img src={require("../images/product-1.jpg").default} alt=""/>
-                <div className="product-hover">
-                    <a href="#" className="add-to-cart-link"><i className="fa fa-shopping-cart"></i> Add to
-                        cart</a>
-                    <a href="single-product.html" className="view-details-link"><i
-                            className="fa fa-link"></i> See details</a>
-                </div>
+              <img src={value.image} alt="" />
+              <div className="product-hover">
+                <a onClick={() => this.addProduct(value)} href="#" className="add-to-cart-link"><i className="fa fa-shopping-cart"></i> Add to
+                  cart</a>
+                <a onClick={() => this.seeDetail(value)} className="view-details-link"><i
+                  className="fa fa-link"></i> See details</a>
+              </div>
             </div>
 
-            <h2><a href="single-product.html">Samsung Galaxy s5- 2015</a></h2>
+            <h2><a>{value.name}</a></h2>
 
             <div className="product-carousel-price">
-                <ins>$700.00</ins> <del>$100.00</del>
+              <ins>{value.discount_price}</ins> <del>{value.actual_price}</del>
             </div>
+          </div>
         </div>
-        )
-    }
+
+      )
+    })
+
+    return (
+      <div class="row">
+        {products}
+      </div>
+    )
+  }
 }
-export default Product;
+
+export default withRouter(Product);
